d3d370b90c129d3e6271773e1bbb2075745e737f
Samir Zafar <chaudharysamirzafar@gmail.com>
2024-08-13 23:55:11 +0100

    Added List All Government Bond Ability to Backend

--- governmentBackend/routes/bonds.js
+++ governmentBackend/routes/bonds.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const staticListOfBonds = require("./staticListOfBonds.json");
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+const { v4: uuidv4 } = require("uuid");
 
 router.get("/", function (req, res, next) {
   res.json(staticListOfBonds);
@@ -24,6 +25,22 @@ router.post("/buy", async function (req, res, next) {
   );
 
   res.json({ issueTokenResponse });
+});
+
+router.get("/list", async function (req, res, next) {
+  const { name } = req.query;
+
+  const { buyerHoldingIdentity } = await getListOfVirtualNodes(name);
+
+  const result = await getListOfGovernmentBondsForUser(
+    buyerHoldingIdentity.shortHash
+  );
+
+  console.log(result);
+
+  const responseJson = JSON.parse(result.flowResult);
+
+  res.json(responseJson);
 });
 
 async function getListOfVirtualNodes(buyerName) {
@@ -98,6 +115,54 @@ async function issueTokenOnLedger(issuer, owner) {
     });
 
   return issueTokenResponse;
+}
+
+async function getListOfGovernmentBondsForUser(userShortHash) {
+  const clientRequestId = "list-" + uuidv4();
+  const requestBody = {
+    clientRequestId: clientRequestId,
+    flowClassName:
+      "com.r3.developers.samples.tokens.workflows.list.ListGovernmentBondTokens",
+    requestBody: {},
+  };
+
+  await fetch(`https://localhost:8888/api/v1/flow/${userShortHash}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Basic YWRtaW46YWRtaW4=",
+    },
+    body: JSON.stringify(requestBody),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const resultOfFlow = await fetch(
+    `https://localhost:8888/api/v1/flow/${userShortHash}/${clientRequestId}`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Basic YWRtaW46YWRtaW4=",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  return resultOfFlow;
 }
 
 module.exports = router;
