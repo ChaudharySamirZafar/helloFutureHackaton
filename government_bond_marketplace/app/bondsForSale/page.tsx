d0baa751f57ef570ffce75eaf988a6da0edaa204
Samir Zafar <chaudharysamirzafar@gmail.com>
2024-08-14 23:05:03 +0100

    Updated Government Marketplace UI

--- government_bond_marketplace/app/bondsForSale/page.tsx
+++ government_bond_marketplace/app/bondsForSale/page.tsx
@@ -1,5 +1,5 @@
-import Bond from "../components/Bond";
 import Navbar from "../components/Navbar";
+import { redirect } from "next/navigation";
 
 export default async function BondsPage() {
   const bonds = await getAllBonds();
@@ -15,6 +15,60 @@ export default async function BondsPage() {
       </div>
     </main>
   );
+}
+
+export function Bond({ bond }: { bond: any }) {
+  const onBuyNowClicked = async () => {
+    "use server";
+
+    await buyBond("Samir", bond.BondID);
+
+    redirect("./myBonds");
+  };
+
+  return (
+    <div className="bg-white shadow-lg w-full rounded-lg px-8 py-4">
+      <h1 className="text-2xl font-bold opacity-60">{bond.BondID}</h1>
+      <h1 className="text-1xl font-semibold">
+        Maturity Date: {bond.MaturityDate}
+      </h1>
+      <h1 className="text-1xl font-semibold">
+        Coupon Rate: {bond.CouponRate}%
+      </h1>
+      <h1 className="text-1xl font-semibold">Face Value: £{bond.FaceValue}</h1>
+      <h1 className="text-md font-bold opacity-60">
+        Currency: {bond.Currency}
+      </h1>
+      <form action={onBuyNowClicked}>
+        <button className="border-2 p-1.5 rounded-md hover:cursor-pointer">
+          Buy Now
+        </button>
+      </form>
+    </div>
+  );
+}
+
+async function buyBond(userName: string, bondId: string) {
+  const requestBody = {
+    buyerName: userName,
+    bondId: bondId,
+  };
+
+  const request = await fetch("http://localhost:4000/bonds/buy", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", // Added Content-Type header
+    },
+    body: JSON.stringify(requestBody),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    });
+
+  return request;
 }
 
 async function getAllBonds() {
--- government_bond_marketplace/app/myBonds/page.tsx
+++ government_bond_marketplace/app/myBonds/page.tsx
@@ -1,7 +1,22 @@
+"use client";
 import Link from "next/link";
 import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
+import { error } from "console";
 
-export default async function BondsPage() {
+export default function BondsPage() {
+  const [myBonds, setMyBonds] = useState([]);
+
+  useEffect(() => {
+    const getAllMyBonds = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      const allMyBonds = await listAllMyBonds("Samir");
+      setMyBonds(allMyBonds);
+    };
+
+    getAllMyBonds();
+  }, []);
+
   return (
     <main className="bg-white min-h-screen w-full flex flex-col items-center">
       <Navbar />
@@ -9,7 +24,34 @@ export default async function BondsPage() {
         <h1 className="text-2xl font-bold">
           Bonds you have successfully purchased
         </h1>
+        <div className="flex flex-col">
+          {myBonds.map((ownedBond: any) => {
+            return (
+              <div className="flex flex-col border-2 px-4 py-6 rounded-lg text-black">
+                <h1>Issuer: {ownedBond.issuerCommonName}</h1>
+                <h1>Owner: {ownedBond.ownerCommonName}</h1>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </main>
   );
+}
+
+async function listAllMyBonds(username: string) {
+  const request = await fetch(
+    `http://localhost:4000/bonds/list?name=${username}`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+  return request;
 }
